5159ab6b8de5e166eb36194b09d7f3d00fc801fe
Isaac <viquezisaac373@gmail.com>
2026-02-05 19:13:05 -0600

    Creación de las primeras clases y configuración del .gitignore

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+// Este avance corresponde al de la semana 1 y 2
+
+// Creación de las clases Producto y Cliente
+public class Producto
+{
+    private int id;
+
+    // Método simple para setear el Id
+    public void SetId(int nuevoId){
+        if(nuevoId  > 0)
+        {
+            id = nuevoId;
+        }
+}
+}
+
+public class Cliente
+{
+    private string nombre;
+
+    // Método simple para setear el nombre
+    public void SetNombre(string nuevoNombre)
+    {
+        nuevoNombre = nombre;
+    }
+
+}
