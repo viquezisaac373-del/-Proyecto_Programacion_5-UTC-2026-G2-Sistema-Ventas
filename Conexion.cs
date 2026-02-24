086c542c45936ef33642d7099900f6f61e4abd92
JonathanVegaH <jvegah0109@uvirtual.ac.cr>
2026-02-23 22:15:09 -0600

    Creacion de clase conexion con base de datos y utilizada en Program para verificar

---
+++ Conexion.cs
@@ -0,0 +1,15 @@
+﻿using MySqlConnector;
+namespace SistemaVentas
+{ 
+    public class Conexion
+    {
+        private string cadenaConexion =
+            "server=localhost;database=facturadb;user=root;password=;"; // en "facturadb" utilizar el mismo nombre que le pusieron a la base de datos 
+        public MySqlConnection ObtenerConexion()
+        {
+            MySqlConnection conexion = new MySqlConnection(cadenaConexion);
+            conexion.Open();
+            return conexion;
+        }
+    }
+}
